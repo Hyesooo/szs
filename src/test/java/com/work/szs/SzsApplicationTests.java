c3282bc08a1f2c1975af697ce9da4a2316276d9f
Hyesooo <hyesookim95@gmail.com>
2024-02-29 14:19:31 +0900

    :sparkles: init project

---
+++ src/test/java/com/work/szs/SzsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.szs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
