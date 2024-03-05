583a88177b6c0ba99768202787f3b123a5592b31
Hyesooo <hyesookim95@gmail.com>
2024-03-05 16:10:10 +0900

    :bug: User_id 유니크 제약조건 추가

--- src/main/java/com/work/szs/user/domain/User.java
+++ src/main/java/com/work/szs/user/domain/User.java
@@ -18,7 +18,7 @@ public class User extends BaseEntity {
     @Column(name = "id", updatable = false)
     private Long id;
 
-    @Column(name = "user_id", nullable = false)
+    @Column(name = "user_id", unique = true, nullable = false)
     private String userId;
 
     @Column(name = "password", nullable = false)
@@ -31,7 +31,6 @@ public class User extends BaseEntity {
     @Column(name = "regNo", nullable = false)
     private String regNo;
 
-    // todo : 이미 가입된 사용자여부(RegNO), UserID 중복 체크
     private User(String userId, String password, String name, String regNo) {
         this.userId = userId;
         this.password = password;
