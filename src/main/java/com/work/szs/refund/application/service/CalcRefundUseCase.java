463dd73fae5b02450f86d2eea0f3191a9173e59d
Hyesooo <hyesookim95@gmail.com>
2024-03-05 16:06:17 +0900

    :bug: 결정세액 계산 후 변화된 결과 DB에 저장

--- src/main/java/com/work/szs/refund/application/service/CalcRefundUseCase.java
+++ src/main/java/com/work/szs/refund/application/service/CalcRefundUseCase.java
@@ -3,21 +3,26 @@ package com.work.szs.refund.application.service;
 import com.work.szs.common.config.TokenService;
 import com.work.szs.common.exception.BusinessInvalidValueException;
 import com.work.szs.refund.application.port.persistence.LoadRefundResultPort;
+import com.work.szs.refund.application.port.persistence.UpdateRefundResultPort;
 import com.work.szs.refund.domain.RefundResult;
 import com.work.szs.user.application.port.out.LoadUserPort;
 import com.work.szs.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CalcRefundUseCase {
     private final TokenService tokenService;
     private final LoadUserPort loadUserPort;
     private final LoadRefundResultPort loadRefundResultPort;
+    private final UpdateRefundResultPort updateRefundResultPort;
 
+    @Transactional
     public Map<String, String> calcRefund() {
         String id = tokenService.getCurrentTokenInfo().getUserId();
         User user = loadUserPort.loadUserByUserId(id).orElseThrow(()
@@ -28,6 +33,10 @@ public class CalcRefundUseCase {
                 () -> new BusinessInvalidValueException("계산을 위한 기초정보가 없습니다.\n 스크래핑 먼저 진행해주세요.")
         );
 
-        return Map.of("결정세액", String.format("%,d", result.calc()));
+        long taxResult = result.calc();
+
+        updateRefundResultPort.saveResult(result);
+
+        return Map.of("결정세액", String.format("%,d", taxResult));
     }
 }
