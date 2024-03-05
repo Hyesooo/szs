57a86fbde9d5f55dc0f6a6bb6d0b924153d608ec
Hyesooo <hyesookim95@gmail.com>
2024-03-05 09:06:14 +0900

    :bug: scrapUseCase 리턴 타입 변경

--- src/main/java/com/work/szs/scrap/application/service/ScarpDataUseCase.java
+++ src/main/java/com/work/szs/scrap/application/service/ScarpDataUseCase.java
@@ -1,5 +1,6 @@
 package com.work.szs.scrap.application.service;
 
+import com.work.szs.scrap.application.dto.command.ScrapDataCommand;
 import com.work.szs.scrap.application.dto.request.ScrapDataRequest;
 import com.work.szs.scrap.application.port.client.ScrapDataPort;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +11,8 @@ import org.springframework.stereotype.Service;
 public class ScarpDataUseCase {
     private final ScrapDataPort scrapDataPort;
 
-    public void inputBaseData(ScrapDataRequest request) {
-        scrapDataPort.getScrapData(request);
+    public ScrapDataCommand inputBaseData(ScrapDataRequest request) {
+        return scrapDataPort.getScrapData(request);
     }
 
 }
